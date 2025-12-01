55dd7e792b9bcb08bbffe4deac48888fe0fc1d31
Halfsa <szendefy.alex@petrik.hu>
2025-12-01 16:00:13 +0100

    music added (JLo)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 import UserInterface.Menu.MenuUI;
 import UserInterface.Window.Window;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         Window window = new Window();
         window.showScreen("menu");
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(new File("./resources/music/Papi.wav")));
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.start();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
